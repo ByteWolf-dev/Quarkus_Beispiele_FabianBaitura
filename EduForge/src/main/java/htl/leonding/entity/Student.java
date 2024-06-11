d346d3f3a277e06e8bf08037db71ffed4142cfbd
Fabian <peacemakerdude3@gmail.com>
2024-06-11 10:14:30 +0200

    adding unique contrainst for firstname and lastname of a student

--- EduForge/src/main/java/htl/leonding/entity/Student.java
+++ EduForge/src/main/java/htl/leonding/entity/Student.java
@@ -7,6 +7,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Table(uniqueConstraints={
+        @UniqueConstraint(columnNames = {"firstName", "lastName"})
+})
 public class Student {
 
     //#region Properties
