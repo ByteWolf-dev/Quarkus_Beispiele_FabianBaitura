ae2b7a4de41c8b84fbc09d0d203336b570447e84
Fabian <peacemakerdude3@gmail.com>
2024-06-12 16:13:00 +0200

    adding post to unit testing

--- WeatherStation/src/main/test/boundary/SensorResourceTest.java
+++ WeatherStation/src/main/test/boundary/SensorResourceTest.java
@@ -2,7 +2,48 @@ package boundary;
 
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
 
 @QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class SensorResourceTest {
+    @Test
+    @Order(1)
+    public void getSensors() {
+        given()
+                .when()
+                .get("/sensor")
+                .then()
+                .log()
+                .body()
+                .statusCode(200)
+                .body("location[0]", is("Location1"),
+                        "name[0]", is("Sensor1")
+                );
+    }
+
+    @Test
+    @Order(2)
+    public void postSensor() {
+        String requestBody = """
+        {
+            "location": "NewLocation",
+            "name": "NewSensor"
+        }
+        """;
+
+        given()
+                .when()
+                .header("Content-Type", "application/json")
+                .body(requestBody)
+                .post("/sensor")
+                .then()
+                .log().body().statusCode(201);
+    }
 }
